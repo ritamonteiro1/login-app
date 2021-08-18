6e78d05a86c5c694b08061a4c445cbe7b9b6db7f
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-18 00:29:38 -0300

    create Widget _body

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -11,69 +11,75 @@ class _LoginViewState extends State<LoginView> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 200,
-                  width: 200,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    child: Image.asset('assets/images/logo.png'),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    border: OutlineInputBorder(),
+                  Container(
+                    height: 20,
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'flutter@flutter.com.br' &&
-                        password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Login errado');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    keyboardType: TextInputType.number,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (email == 'flutter@flutter.com.br' &&
+                          password == '123') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('Login errado');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: _body());
   }
 }
