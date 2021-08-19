c0c924e82b61127d5fd8c688ea71d2e76cef6706
ritamonteiro1 <ritamonteiro@tokenlab.com.br>
2021-08-19 16:57:01 -0300

    change in _LoginViewState

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -14,76 +14,74 @@ class _LoginViewState extends State<LoginView> {
   Widget _body() {
     return Column(
       children: [
-        SingleChildScrollView(
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 200,
-                    width: 200,
-                    child: Image.asset('assets/images/logo.png'),
-                  ),
-                  Container(
-                    height: 20,
-                  ),
-                  Card(child: Padding(
-                    padding: const EdgeInsets.only(left: 12, right: 12, top: 20, bottom: 12),
-                    child: Column(
-                      children: [
-                        TextField(
-                          onChanged: (text) {
-                            email = text;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            labelText: 'Email',
-                            border: OutlineInputBorder(),
-                          ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                Container(
+                  height: 20,
+                ),
+                Card(child: Padding(
+                  padding: const EdgeInsets.only(left: 12, right: 12, top: 20, bottom: 12),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
                         ),
-                        SizedBox(
-                          height: 10,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        keyboardType: TextInputType.number,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          border: OutlineInputBorder(),
                         ),
-                        TextField(
-                          onChanged: (text) {
-                            password = text;
-                          },
-                          keyboardType: TextInputType.number,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Senha',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),SizedBox(
-                          height: 15,
+                      ),SizedBox(
+                        height: 15,
+                      ),
+                      RaisedButton(
+                        textColor: Colors.white,
+                        color: Colors.red,
+                        onPressed: () {
+                          if (email == 'flutter@flutter.com.br' &&
+                              password == '123') {
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print('Login errado');
+                          }
+                        },
+                        child: Container(
+                            width: double.infinity,
+                            child: Text('Entrar',
+                              textAlign: TextAlign.center,),
                         ),
-                        RaisedButton(
-                          textColor: Colors.white,
-                          color: Colors.red,
-                          onPressed: () {
-                            if (email == 'flutter@flutter.com.br' &&
-                                password == '123') {
-                              Navigator.of(context).pushReplacementNamed('/home');
-                            } else {
-                              print('Login errado');
-                            }
-                          },
-                          child: Container(
-                              width: double.infinity,
-                              child: Text('Entrar',
-                                textAlign: TextAlign.center,),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                  ),
-                ],
-              ),
+                ),
+                ),
+              ],
             ),
           ),
         ),
